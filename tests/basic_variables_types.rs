b373fe07f77cbbb3232316a8b68d55a4f9a6f519
Finn Brewer <weeaseled@gmail.com>
2024-03-02 12:49:54 -0800

    feat: add tests for inferred variables

--- tests/basic_variables_types.rs
+++ tests/basic_variables_types.rs
@@ -7,6 +7,35 @@ fn test_creating_str_var() {
     let mut file = bashtyped::FileInfo::new(r#"a="lol" #/ string"#);
     file.parse_code();
     assert!(file.errors.is_empty());
+    assert_eq!(
+        file.variables,
+        HashMap::from([(
+            String::from("a"),
+            TypeDeclaration {
+                bash_type: BashType::String,
+                method: Method::Declared,
+                range: 0..17,
+            },
+        )])
+    );
+}
+
+#[test]
+fn test_creating_inferred_str_var() {
+    let mut file = bashtyped::FileInfo::new(r#"a="lol""#);
+    file.parse_code();
+    assert!(file.errors.is_empty());
+    assert_eq!(
+        file.variables,
+        HashMap::from([(
+            String::from("a"),
+            TypeDeclaration {
+                bash_type: BashType::String,
+                method: Method::Inferred,
+                range: 0..7,
+            },
+        )])
+    );
 }
 
 #[test]
@@ -46,5 +75,67 @@ a=1"#,
                 range: 1..11,
             },
         )])
+    );
+}
+
+#[test]
+fn test_creating_multiple_int_vars() {
+    let mut file = bashtyped::FileInfo::new(
+        r#"a=1 #/ int
+b=3 #/ int"#,
+    );
+    file.parse_code();
+    assert!(file.errors.is_empty());
+    assert_eq!(
+        file.variables,
+        HashMap::from([
+            (
+                String::from("a"),
+                TypeDeclaration {
+                    bash_type: BashType::Integer,
+                    method: Method::Declared,
+                    range: 0..10,
+                },
+            ),
+            (
+                String::from("b"),
+                TypeDeclaration {
+                    bash_type: BashType::Integer,
+                    method: Method::Declared,
+                    range: 11..21,
+                },
+            )
+        ])
+    );
+}
+
+#[test]
+fn test_creating_multiple_int_vars_first_inferred() {
+    let mut file = bashtyped::FileInfo::new(
+        r#"a=1
+b=3 #/ int"#,
+    );
+    file.parse_code();
+    assert!(file.errors.is_empty());
+    assert_eq!(
+        file.variables,
+        HashMap::from([
+            (
+                String::from("a"),
+                TypeDeclaration {
+                    bash_type: BashType::Integer,
+                    method: Method::Inferred,
+                    range: 0..3,
+                },
+            ),
+            (
+                String::from("b"),
+                TypeDeclaration {
+                    bash_type: BashType::Integer,
+                    method: Method::Declared,
+                    range: 4..14,
+                },
+            )
+        ])
     );
 }
